f4dc8dae644e8b15aea2dba806c1e9b58f2ad43f
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-23 06:21:30 -0800

    Add files via upload

---
+++ pyrobot/plugins/test_pyro_plugin.py
@@ -0,0 +1,15 @@
+"""test module"""
+
+from pyrogram import Client, Filters
+
+
+from pyrobot import (
+    MAX_MESSAGE_LENGTH,
+    COMMAND_HAND_LER,
+    LOGGER
+)
+
+
+@Client.on_message(Filters.command("testcommand", COMMAND_HAND_LER)  & Filters.me)
+async def _(client, message):
+    await message.edit("test ing ... failed")
