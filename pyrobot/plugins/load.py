90c0d1bc4479496d6fde0baeb4df5cb93fc77564
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-02-23 01:34:44 -0800

    Add files via upload

---
+++ pyrobot/plugins/load.py
@@ -0,0 +1,66 @@
+""" The core 'pyrobot' module"""
+
+from importlib import import_module, reload
+import os,logging
+from pathlib import Path
+from pyrogram import Client, Filters
+from pyrogram.client.handlers.handler import Handler
+
+logging.basicConfig(level=logging.DEBUG,format='%(message)s')
+logger = logging.getLogger()
+
+from creds import Config
+
+@Client.on_message(Filters.command("load", Config.COMMAND_HAND_LER)  & Filters.me)
+async def load_plugin(client, message):
+    await message.edit("Processing ...")
+    try:
+        if message.reply_to_message is not None:
+            down_loaded_plugin_name = await message.reply_to_message.download(
+                file_name="./plugins/"
+            )
+            if down_loaded_plugin_name is not None:
+                # LOGGER.info(down_loaded_plugin_name)
+                relative_path_for_dlpn = os.path.relpath(
+                    down_loaded_plugin_name,
+                    os.getcwd()
+                )
+                # LOGGER.info(relative_path_for_dlpn)
+                lded_count = 0
+                path = Path(relative_path_for_dlpn)
+                module_path = ".".join(
+                    path.parent.parts + (path.stem,)
+                )
+                # LOGGER.info(module_path)
+                module = reload(import_module(module_path))
+                # https://git.io/JvlNL
+                for name in vars(module).keys():
+                    # noinspection PyBroadException
+                    try:
+                        handler, group = getattr(module, name).handler
+    
+                        if isinstance(handler, Handler) and isinstance(group, int):
+                            client.add_handler(handler, group)
+                            logger.info(
+                                '[{}] [LOAD] {}("{}") in group {} from "{}"'.format(
+                                    client.session_name,
+                                    type(handler).__name__,
+                                    name,
+                                    group,
+                                    module_path
+                                )
+                            )
+    
+                            lded_count += 1
+                    except Exception as e:
+                        # LOGGER.info(str(e))
+                        pass
+                await message.edit(
+                    f"installed {lded_count} commands / plugins"
+                )
+    except Exception as error:
+        await message.edit(
+             f"ERROR: `{error}`"
+        )
+
+  
